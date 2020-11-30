fe219755083ac0761fd5da2c785f9c089d9a70e0
grantabejar <73010478+grantabejar@users.noreply.github.com>
2020-11-30 00:19:20 -0500

    Update captcha.sh

--- captcha.sh
+++ captcha.sh
@@ -1,43 +1,20 @@
 #!/bin/bash
 
 cat << "EOF"
-        _____                        __                                         
-  _____/ ____\        _____   __ ___/  |_  ____            __ _______    _____  
-_/ ___\   __\  ______ \__  \ |  |  \   __\/  _ \   ______ |  |  \__  \  /     \ 
-\  \___|  |   /_____/  / __ \|  |  /|  | (  <_> ) /_____/ |  |  // __ \|  Y Y  \
- \___  >__|           (____  /____/ |__|  \____/          |____/(____  /__|_|  /
-     \/                    \/                                        \/      \/ 
+Created by securer of The Lucidire
 EOF
 
-printf "\033[1;34mWelcome to the cf-auto-uam installer.\r\nVersion 1.0\033[0m"
+printf "\033[1;34mWelcome to the Auto-captcha.\r\nVersion 1.0\033[0m"
 printf "\r\n\r\n"
 
-if [ -d "/root/cf-auto-uam/" ]; then
-  printf "\033[1;31mDetected previous installation of cf-auto-uam.\r\n"
+if [ -d "/root/cap/" ]; then
+  printf "\033[1;31mDetected previous installation of cap.\r\n"
   sleep .5
   printf "Removing it...\r\n\033[0m"
-  rm -rf /root/cf-auto-uam
+  rm -rf /root/cap
 fi
 
 sleep .5
-
-printf "\033[1;34m\nCloudFlare E-Mail (example@mail.com) :\n> \033[0m"
-read cfemail
-
-emailregex="^([A-Za-z]+[A-Za-z0-9]*((\.|\-|\_)?[A-Za-z]+[A-Za-z0-9]*){1,})@(([A-Za-z]+[A-Za-z0-9]*)+((\.|\-|\_)?([A-Za-z]+[A-Za-z0-9]*)+){1,})+\.([A-Za-z]{2,})+"
-
-if ! [[ $cfemail =~ $emailregex ]] ; then
-
-	printf "\033[1;31mInvalid email.\r\n\033[0m"
-	exit
-
-fi
-
-printf "\033[1;34m\nCloudFlare API Key :\n> \033[0m"
-read cfapikey
-
-printf "\033[1;34m\nCloudFlare Zone ID :\n> \033[0m"
-read cfzoneid
 
 if [[ -e /etc/debian_version ]]; then
 
@@ -54,8 +31,8 @@ if [[ -e /etc/debian_version ]]; then
 	printf "\033c"
 	printf "\033[1;32mBuilding files.\r\n\033[0m"
 	sleep 1
-	mkdir /root/cf-auto-uam
-	cd /root/cf-auto-uam
+	mkdir /root/cap
+	cd /root/cap
 cat > activate.sh << EOF
 #!/bin/bash
 loadavg=load.avg
@@ -65,7 +42,7 @@ if [ \$? -eq 0 ]
 then
 exit
 else
-cd ~ && vddos-switch allsite captcha
+bash yes.sh
 fi
 EOF
 cat > deactivate.sh << EOF
@@ -75,51 +52,45 @@ cat /proc/loadavg | colrm 6 > \$loadavg
 grep -w "[0.00-20.00]" \$loadavg > /dev/null
 if [ \$? -eq 0 ]
 then
-cd ~ && vddos-switch allsite no
+bash no.sh
 else
 exit
 fi
 EOF
-cat > high.sh << EOF
-curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$cfzoneid/settings/security_level" \\
-     -H "X-Auth-Email: $cfemail" \\
-     -H "X-Auth-Key: $cfapikey" \\
-     -H "Content-Type: application/json" \\
-     --data '{"value":"low"}'
+cat > no.sh << EOF
+(cd ~ &) && (vddos-switch allsite no &)
+
 EOF
-cat > uam.sh << EOF
-curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$cfzoneid/settings/security_level" \\
-     -H "X-Auth-Email: $cfemail" \\
-     -H "X-Auth-Key: $cfapikey" \\
-     -H "Content-Type: application/json" \\
-     --data '{"value":"under_attack"}'
+cat > yes.sh << EOF
+(cd ~ &) && (vddos-switch allsite captcha &)
+
 EOF
 
-	touch /root/cf-auto-uam/load.avg
-	chmod 500 /root/cf-auto-uam/
-	chmod 500 /root/cf-auto-uam/activate.sh
-	chmod 500 /root/cf-auto-uam/deactivate.sh
-	chmod 500 /root/cf-auto-uam/high.sh
-	chmod 500 /root/cf-auto-uam/uam.sh
-	chmod 600 /root/cf-auto-uam/load.avg
+	touch /root/cap/load.avg
+	chmod 500 /root/cap/
+	chmod 500 /root/cap/activate.sh
+	chmod 500 /root/cap/deactivate.sh
+	chmod 500 /root/cap/no.sh
+	chmod 500 /root/cap/yes.sh
+	chmod 600 /root/cap/load.avg
 	crontab -l > cron1
-	echo "* * * * * cd /root/cf-auto-uam/ ; bash activate.sh" >> cron1
+	echo "* * * * * cd /root/cap/ ; bash activate.sh" >> cron1
 	crontab cron1
 	rm cron1
 	crontab -l > cron2
-	echo "* * * * * cd /root/cf-auto-uam/ ; sleep 15 ; bash activate.sh" >> cron2
+	echo "* * * * * cd /root/cap/ ; sleep 15 ; bash activate.sh" >> cron2
 	crontab cron2
 	rm cron2
 	crontab -l > cron3
-	echo "* * * * * cd /root/cf-auto-uam/ ; sleep 30 ; bash activate.sh" >> cron3
+	echo "* * * * * cd /root/cap/ ; sleep 30 ; bash activate.sh" >> cron3
 	crontab cron3
 	rm cron3
 	crontab -l > cron4
-	echo "* * * * * cd /root/cf-auto-uam/ ; sleep 45 ; bash activate.sh" >> cron4
+	echo "* * * * * cd /root/cap/ ; sleep 45 ; bash activate.sh" >> cron4
 	crontab cron4
 	rm cron4
 	crontab -l > cron5
-	echo "10,30 * * * * cd /root/cf-auto-uam/ ; bash deactivate.sh" >> cron5
+	echo "10,30 * * * * cd /root/cap/ ; bash deactivate.sh" >> cron5
 	crontab cron5
 	rm cron5
 	service cron restart
@@ -139,8 +110,8 @@ elif [[ -e /etc/centos-release ]]; then
 	printf "\033c"
 	printf "\033[1;32mBuilding files.\r\n\033[0m"
 	sleep 1
-	mkdir /root/cf-auto-uam
-	cd /root/cf-auto-uam
+	mkdir /root/cap
+	cd /root/cap
 cat > activate.sh << EOF
 #!/bin/bash
 loadavg=load.avg
@@ -150,7 +121,7 @@ if [ \$? -eq 0 ]
 then
 exit
 else
-cd ~ && vddos-switch allsite captcha
+bash yes.sh
 fi
 EOF
 cat > deactivate.sh << EOF
@@ -160,51 +131,45 @@ cat /proc/loadavg | colrm 6 > \$loadavg
 grep -w "[0.00-20.00]" \$loadavg > /dev/null
 if [ \$? -eq 0 ]
 then
-cd ~ && vddos-switch allsite no
+bash no.sh
 else
 exit
 fi
 EOF
-cat > high.sh << EOF
-curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$cfzoneid/settings/security_level" \\
-     -H "X-Auth-Email: $cfemail" \\
-     -H "X-Auth-Key: $cfapikey" \\
-     -H "Content-Type: application/json" \\
-     --data '{"value":"low"}'
+cat > no.sh << EOF
+(cd ~ &) && (vddos-switch allsite no &)
+
 EOF
-cat > uam.sh << EOF
-curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$cfzoneid/settings/security_level" \\
-     -H "X-Auth-Email: $cfemail" \\
-     -H "X-Auth-Key: $cfapikey" \\
-     -H "Content-Type: application/json" \\
-     --data '{"value":"under_attack"}'
+cat > yes.sh << EOF
+(cd ~ &) && (vddos-switch allsite captcha &)
+
 EOF
 
-	touch /root/cf-auto-uam/load.avg
-	chmod 500 /root/cf-auto-uam/
-	chmod 500 /root/cf-auto-uam/activate.sh
-	chmod 500 /root/cf-auto-uam/deactivate.sh
-	chmod 500 /root/cf-auto-uam/high.sh
-	chmod 500 /root/cf-auto-uam/uam.sh
-	chmod 600 /root/cf-auto-uam/load.avg
+	touch /root/cap/load.avg
+	chmod 500 /root/cap/
+	chmod 500 /root/cap/activate.sh
+	chmod 500 /root/cap/deactivate.sh
+	chmod 500 /root/cap/no.sh
+	chmod 500 /root/cap/yes.sh
+	chmod 600 /root/cap/load.avg
 	crontab -l > cron1
-	echo "* * * * * cd /root/cf-auto-uam/ ; bash activate.sh" >> cron1
+	echo "* * * * * cd /root/cap/ ; bash activate.sh" >> cron1
 	crontab cron1
 	rm cron1
 	crontab -l > cron2
-	echo "* * * * * cd /root/cf-auto-uam/ ; sleep 15 ; bash activate.sh" >> cron2
+	echo "* * * * * cd /root/cap/ ; sleep 15 ; bash activate.sh" >> cron2
 	crontab cron2
 	rm cron2
 	crontab -l > cron3
-	echo "* * * * * cd /root/cf-auto-uam/ ; sleep 30 ; bash activate.sh" >> cron3
+	echo "* * * * * cd /root/cap/ ; sleep 30 ; bash activate.sh" >> cron3
 	crontab cron3
 	rm cron3
 	crontab -l > cron4
-	echo "* * * * * cd /root/cf-auto-uam/ ; sleep 45 ; bash activate.sh" >> cron4
+	echo "* * * * * cd /root/cap/ ; sleep 45 ; bash activate.sh" >> cron4
 	crontab cron4
 	rm cron4
 	crontab -l > cron5
-	echo "10,30 * * * * cd /root/cf-auto-uam/ ; bash deactivate.sh" >> cron5
+	echo "10,30 * * * * cd /root/cap/ ; bash deactivate.sh" >> cron5
 	crontab cron5
 	rm cron5
 	service crond restart
@@ -214,6 +179,6 @@ else
 	exit 1
 fi
 
-printf "\033[1;32mInstallation finished successfully without any errors!\033[0m"
+printf "\033[1;32mInstallation finished successfully without any errors! Please visit us at lucidire.com\033[0m"
 printf "\r\n\r\n"
 exit
